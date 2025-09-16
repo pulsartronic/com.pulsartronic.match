c224affb2a9032b4b26cfb1ea47952cf85926a0d
Marcelo <dev.pulsartronic@gmail.com>
2025-09-16 17:36:45 +0200

    wip

--- Runtime/Environment/GameLimits/GameLimits.cs
+++ Runtime/Environment/GameLimits/GameLimits.cs
@@ -6,4 +6,5 @@ public class GameLimits : MonoBehaviour {
     public FieldLimits line_out_limits;
     public FieldLimits restart_null_limits;
     public FieldLimits scrum_limits;
+    public FieldLimits ingoal_limits;
 }
